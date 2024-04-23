ab012bb4ad285b74463fc379a47d8ef2d17f8d7c
Pedro H R C <106977723+1221pepo1221@users.noreply.github.com>
2024-04-23 16:21:53 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,9 +15,18 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-05-05T00:00:00");
+const tempoObjetivo1 = new Date("2024-11-18T00:00:00");
+const tempoObjetivo2 = new Date("2024-05-11T00:00:00");
+const tempoObjetivo3 = new Date("2024-05-31T00:00:00");
+const tempoObjetivo4 = new Date("2024-03-10T00:00:00");
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4,];
 
-
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+ 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo) {
